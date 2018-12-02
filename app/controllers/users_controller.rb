a9bfc8331b08af436f0fccb083ce246d91114711
Aivaz Miftakhov <vazovskee@gmail.com>
2018-12-02 16:56:16 +0300

    views refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,40 @@
 class UsersController < ApplicationController
-
-  def show
-    @hello = 'Привет мир'
+  def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Vadim',
+        username: 'installero',
+        avatar_url: 'https://secure.gravatar.com/avatar/'\
+              '71269686e0f757ddb4f73614f43ae445?s=100'
+      ),
+      User.new(
+        id: 2,
+        name: 'Misha',
+        username: 'aristofun'
+      )
+    ]
   end
 
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+    @user = User.new(
+      name: 'Vadim',
+      username: 'installero',
+      avatar_url: 'https://secure.gravatar.com/avatar/'\
+                  '71269686e0f757ddb4f73614f43ae445?s=100'
+    )
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('02.12.2018')),
+      Question.new(text: 'В чём смысл жизни?', created_at: Date.parse('01.12.2018'))
+    ]
+
+    @new_question = Question.new
+  end
 end
