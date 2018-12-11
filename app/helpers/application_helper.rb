12ad07d56ed7313ff655fc06b9b1bf9e37ddd174
Aivaz Miftakhov <vazovskee@gmail.com>
2018-12-11 12:05:48 +0300

    question controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,10 @@ module ApplicationHelper
     end
   end
 
+  def fa_icon(icon_class)
+    content_tag 'span', '', class: "fa fa-#{icon_class}"
+  end
+
   def correct_declension(number, declensions)
     return declensions[:third_declension] if (number % 100).between?(11, 14)
 
